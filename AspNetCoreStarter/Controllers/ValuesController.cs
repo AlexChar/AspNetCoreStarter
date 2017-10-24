1d16f3e0e23bc883f79376743d0a9439c3610078
ppolyzos <ppolyzos@gmail.com>
2017-10-24 19:53:46 +0300

    feat(values): simple routing validation attributes

---
+++ AspNetCoreStarter/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreStarter.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new [] { "value1", "value2" });
+        }
+
+        [HttpGet("{id:min(5)}")]
+        public IActionResult Get(int id)
+        {
+            return Ok($"value{id}");
+        }
+    }
+}
