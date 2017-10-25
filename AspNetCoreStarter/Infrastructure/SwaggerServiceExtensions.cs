ba5dc0ae31b94ddcfc029c76a4c9652111f3a2ac
ppolyzos <ppolyzos@gmail.com>
2017-10-25 22:25:04 +0300

    feat(docs): add jwt support in swagger

--- AspNetCoreStarter/Infrastructure/SwaggerServiceExtensions.cs
+++ AspNetCoreStarter/Infrastructure/SwaggerServiceExtensions.cs
@@ -11,6 +11,14 @@ namespace AspNetCoreStarter.Infrastructure
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1.0", new Info { Title = "Main API v1.0", Version = "v1.0" });
+
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
             });
 
             return services;
